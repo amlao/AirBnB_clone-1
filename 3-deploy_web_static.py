d7b46c4674175b65534ec3d4fb1086c56d659395
Anthony la'O <anthonylao@live.com>
2018-07-30 22:16:41 +0000

    Test for checkers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,13 +3,8 @@
 a Fabric script (based on the file 2-do_deploy_web_static.py) that creates
 and distributes an archive to your web servers, using the function deploy
 """
-from fabric.api import *
-import os.path
-from fabric.operations import run, put, sudo
 do_pack = __import__('1-pack_web_static')
 do_deploy = __import__('2-do_deploy_web_static')
-env.hosts = ['35.196.149.169', '35.185.80.112']
-env.user = 'ubuntu'
 
 
 def deploy():
