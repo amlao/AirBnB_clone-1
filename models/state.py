086e445a1ae78c159e9bee4a65a64651863f96b5
Anthony la'O <anthonylao@live.com>
2018-08-15 08:58:08 +0000

    Reverted back to old code

--- models/state.py
+++ models/state.py
@@ -5,24 +5,22 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
+import os
+import models
 
 
 class State(BaseModel, Base):
     '''
         Implementation for the State.
     '''
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    city = relationship("City", backref="state", cascade="delete")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        city = relationship("City", backref="state", cascade="delete")
 
     @property
     def cities(self):
         """
             a public getter for cities
         """
-        if getenv('HBNB_TYPE_STORAGE') == 'db':
-            city = relationship("City", backref="state", cascade="delete")
-        else:
-            city_list = models.storage.all(City)
-            return [apple for big, apple in city_list.items()
-                    if apple.state_id == self.id]
+        return [city for city in State.cities if city.state_id == self.id]
